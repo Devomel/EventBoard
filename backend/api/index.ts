a978127a18f0a13b17ff9b0444f5a2ce877f1884
Devomel <omellwork@gmail.com>
2024-09-25 16:00:05 +0300

    export app

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -9,24 +9,24 @@ app.use(express.json());
 
 const mongoURI = process.env.DB_URL;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 const start = () => {
   try {
     if (!mongoURI) {
       throw new Error('DB_URL is not defined in the environment variables');
     }
 
-    app.get('/', (req: Request, res: Response) => {
-      res.send('Hello, TypeScript with Express!');
-    });
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
     mongoose.connect(mongoURI)
   } catch (error) {
     console.log(error)
   }
 }
 
-module.exports = start;
+module.exports = app;
