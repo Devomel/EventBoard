bb4125da209cfc72ca32651885e06ef4d158a144
Devomel <omellwork@gmail.com>
2024-09-25 14:07:08 +0300

    configuring

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,12 +1,31 @@
 import express, { Request, Response } from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
 const app = express();
 const PORT = process.env.PORT || 3000;
+dotenv.config();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const mongoURI = process.env.DB_URL;
+
+
+const start = async () => {
+  try {
+    if (!mongoURI) {
+      throw new Error('DB_URL is not defined in the environment variables');
+    }
+    await mongoose.connect(mongoURI)
+    app.get('/', (req: Request, res: Response) => {
+      res.send('Hello, TypeScript with Express!');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
