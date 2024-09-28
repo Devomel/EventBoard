a9bc7cb958115d0964dd7ab0e6102a06136b9d29
Devomel <omellwork@gmail.com>
2024-09-28 18:06:19 +0300

    complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
+import AppRouter from "./components/AppRouter"
 
 
 function App() {
-
-
   return (
-    <>
-      asfg
-    </>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   )
 }
 
